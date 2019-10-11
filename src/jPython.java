0103dfb13bfd9dba736462a2f2015c68bac3f67d
wushiwuwang <346414322@qq.com>
2019-10-11 16:30:41 +0800

    添加解压文件功能

--- src/jPython.java
+++ src/jPython.java
@@ -1,6 +1,17 @@
+import jpython.PreprocessedData;
 
 public class jPython {
     public static void main(String[] args) throws Exception {
+        PreprocessedData pre =  new PreprocessedData();
+       // pre.unZip("H:\\c.zip", "H:\\test-1");
+        //pre.unTarGz("D:\\c.tar.gz","D:\\");
+        String file1 = "D:\\c.tar.gz";
+        String file2 = "D:\\";
+        pre.unTarGz(file1,file2);
+//        ArrayList<String> list = new ArrayList<>();
+//        String parentFile = "H:\\test-1";
+//
+//        pre.functionSplit("H:\\test-1","clang-tidy");
 
     }
 }
--- src/jpython/PreprocessedData.java
+++ src/jpython/PreprocessedData.java
@@ -1,26 +1,192 @@
 package jpython;
 
+import com.ice.tar.TarEntry;
+import com.ice.tar.TarInputStream;
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 public class PreprocessedData {
 
     public PreprocessedData() {
     }
+    //解压.zip
+    public void unZip(String file, String outputDir) throws IOException {
+        ZipFile zipFile = null;
+        System.out.println(file);
+        try {
+            Charset CP866 = Charset.forName("CP866");  //specifying alternative (non UTF-8) charset
+            //ZipFile zipFile = new ZipFile(zipArchive, CP866);
+            zipFile =  new ZipFile(file, CP866);
+            createDirectory(outputDir,null);//创建输出目录
 
-    public String decode() {
-        String project_name = null;
-        //解压代码
-        return project_name;
+            Enumeration<?> enums = zipFile.entries();
+            while(enums.hasMoreElements()){
+
+                ZipEntry entry = (ZipEntry) enums.nextElement();
+                System.out.println("解压." +  entry.getName());
+
+                if(entry.isDirectory()){//是目录
+                    createDirectory(outputDir,entry.getName());//创建空目录
+                }else{//是文件
+                    File tmpFile = new File(outputDir + "/" + entry.getName());
+                    createDirectory(tmpFile.getParent() + "/",null);//创建输出目录
+
+                    InputStream in = null;
+                    OutputStream out = null;
+                    try{
+                        in = zipFile.getInputStream(entry);;
+                        out = new FileOutputStream(tmpFile);
+                        int length = 0;
+
+                        byte[] b = new byte[2048];
+                        while((length = in.read(b)) != -1){
+                            out.write(b, 0, length);
+                        }
+
+                    }catch(IOException ex){
+                        throw ex;
+                    }finally{
+                        if(in!=null)
+                            in.close();
+                        if(out!=null)
+                            out.close();
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            throw new IOException("解压缩文件出现异常",e);
+        } finally{
+            try{
+                if(zipFile != null){
+                    zipFile.close();
+                }
+            }catch(IOException ex){
+                throw new IOException("关闭zipFile出现异常",ex);
+            }
+        }
+    }
+    //解压.tar.gz
+    public void unTarGz(String file, String outputDir) throws IOException{
+        TarInputStream tarIn = null;
+        try{
+            tarIn = new TarInputStream(new GZIPInputStream(
+                    new BufferedInputStream(new FileInputStream(file))),
+                    1024 * 2);
+
+            createDirectory(outputDir,null);//创建输出目录
+
+            TarEntry entry = null;
+            while( (entry = tarIn.getNextEntry()) != null ){
+
+                if(entry.isDirectory()){//是目录
+                    entry.getName();
+                    createDirectory(outputDir,entry.getName());//创建空目录
+                }else{//是文件
+                    File tmpFile = new File(outputDir + "/" + entry.getName());
+                    createDirectory(tmpFile.getParent() + "/",null);//创建输出目录
+                    OutputStream out = null;
+                    try{
+                        out = new FileOutputStream(tmpFile);
+                        int length = 0;
+
+                        byte[] b = new byte[2048];
+
+                        while((length = tarIn.read(b)) != -1){
+                            out.write(b, 0, length);
+                        }
+
+                    }catch(IOException ex){
+                        throw ex;
+                    }finally{
+
+                        if(out!=null)
+                            out.close();
+                    }
+                }
+            }
+        }catch(IOException ex){
+            throw new IOException("解压归档文件出现异常",ex);
+        } finally{
+            try{
+                if(tarIn != null){
+                    tarIn.close();
+                }
+            }catch(IOException ex){
+                throw new IOException("关闭tarFile出现异常",ex);
+            }
+        }
+    }
+    //解压.tar
+    public void unTar(String file, String outputDir){
+        FileInputStream fis = null;
+        OutputStream fos = null;
+        TarInputStream tarInputStream = null;
+        try {
+            fis = new FileInputStream(file);
+            tarInputStream = new TarInputStream(fis, 1024 * 2);
+            // 创建输出目录
+            createDirectory(outputDir, null);
+
+            TarEntry entry = null;
+            while(true){
+                entry = tarInputStream.getNextEntry();
+                if( entry == null){
+                    break;
+                }
+                if(entry.isDirectory()){
+                    createDirectory(outputDir, entry.getName()); // 创建子目录
+                }else{
+                    fos = new FileOutputStream(new File(outputDir + "/" + entry.getName()));
+                    int count;
+                    byte data[] = new byte[2048];
+                    while ((count = tarInputStream.read(data)) != -1) {
+                        fos.write(data, 0, count);
+                    }
+                    fos.flush();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if(fis != null){
+                    fis.close();
+                }
+                if(fos != null){
+                    fos.close();
+                }
+                if(tarInputStream != null){
+                    tarInputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+   //构建目录
+    public void createDirectory(String outputDir,String subDir){
+        File file = new File(outputDir);
+        if(!(subDir == null || subDir.trim().equals(""))){//子目录不为空
+            file = new File(outputDir + "/" + subDir);
+        }
+        if(!file.exists()){
+            if(!file.getParentFile().exists())
+                file.getParentFile().mkdirs();
+            file.mkdirs();
+        }
     }
 
     public void functionSplit(String dir, String project_name) {
